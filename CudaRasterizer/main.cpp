789030322e7cc5450671af2a71bfca5517868e56
TomStevens7533 <tom.stevens2@student.howest.be>
2022-10-23 12:23:43 +0200

    A - Added GLFW/GLEW/GLM

--- CudaRasterizer/main.cpp
+++ CudaRasterizer/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <gl/GL.h>
+#include <glm/glm.hpp>
+
+void InitGLFW();
 
 int main() {
+	InitGLFW();
 	std::cout << "Hello world\n";
+}
+
+
+void InitGLFW()
+{
+	if (!glfwInit())
+	{
+		// Initialization failed
+		std::cout << "glfw failed initialization!\n";
+	}
 }
