e5d79c09325d0befaa4206684c12e7c4931a9135
TomStevens7533 <tom.stevens2@student.howest.be>
2022-11-27 12:51:06 +0100

    U -Added initialization of CUDA/GLEW

--- CudaRasterizer/kernel.cu
+++ CudaRasterizer/kernel.cu
@@ -1,87 +1,87 @@
-﻿//
-//#include "cuda_runtime.h"
-//#include "device_launch_parameters.h"
-//
-//#include <stdio.h>
-//#include <chrono>
-//#include <iostream>
-//
-//using namespace std::chrono;
-//
-//void add(int n, float* x, float* y) 
-//{
-//
-//	for (size_t i = 0; i < n; i++)
-//	{
-//		y[i] = x[i] + y[i];
-//	}
-//}
-//
-//__global__
-//void parallelAdd(int n, float* x, float* y)
-//{
-//	int index = threadIdx.x;
-//	int stride = blockDim.x;
-//
-//	for (size_t i = index; i < n; i+= stride)
-//	{
-//		y[i] = x[i] + y[i];
-//
-//	}
-//}
-//
-//int main() {
-//	int N = 1 << 20;
-//
-//	//Alocate cpu only memory
-//	float* x = new float[N];
-//	float* y = new float[N];
-//
-//	for (size_t i = 0; i < N; i++)
-//	{
-//		x[i] = 1.0f;
-//		y[i] = 2.0f;
-//	}
-//	//Allocate shared unified memory with GPU
-//
-//	float* Parx;
-//	float* Pary;
-//	cudaMallocManaged(&Parx, N * sizeof(float));
-//	cudaMallocManaged(&Pary, N * sizeof(float));
-//
-//	for (size_t i = 0; i < N; i++)
-//	{
-//		Parx[i] = 1.0f;
-//		Pary[i] = 2.0f;
-//	}
-//	
-//
-//	auto start = high_resolution_clock::now();
-//	add(N, x, y);
-//	auto stop = high_resolution_clock::now();
-//	auto duration = duration_cast<microseconds>(stop - start);
-//	std::cout << "Time taken by non-parralel function: "
-//		<< duration.count() << " microseconds" << std::endl;
-//
-//
-//	start = high_resolution_clock::now();
-//	
-//
-//	//<<<N,X>>> N = amount of threadblocks; X = amount of threads in a block;
-//	parallelAdd<<<1,N>>>(N, x, y);
-//	cudaDeviceSynchronize();
-//	stop = high_resolution_clock::now();
-//	duration = duration_cast<microseconds>(stop - start);
-//	std::cout << "Time taken by parralel function: "
-//		<< duration.count() << " microseconds" << std::endl;
-//
-//	delete[] x;
-//	delete[] y;
-//
-//	//free cuda memory wait till kernel is done
-//	cudaFree(Parx);
-//	cudaFree(Pary);
-//
-//
-//
-//}
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <chrono>
+#include <iostream>
+
+using namespace std::chrono;
+
+void add(int n, float* x, float* y) 
+{
+
+	for (size_t i = 0; i < n; i++)
+	{
+		y[i] = x[i] + y[i];
+	}
+}
+
+__global__
+void parallelAdd(int n, float* x, float* y)
+{
+	int index = threadIdx.x;
+	int stride = blockDim.x;
+
+	for (size_t i = index; i < n; i+= stride)
+	{
+		y[i] = x[i] + y[i];
+
+	}
+}
+
+int dsdamain() {
+	int N = 1 << 20;
+
+	//Alocate cpu only memory
+	float* x = new float[N];
+	float* y = new float[N];
+
+	for (size_t i = 0; i < N; i++)
+	{
+		x[i] = 1.0f;
+		y[i] = 2.0f;
+	}
+	//Allocate shared unified memory with GPU
+
+	float* Parx;
+	float* Pary;
+	cudaMallocManaged(&Parx, N * sizeof(float));
+	cudaMallocManaged(&Pary, N * sizeof(float));
+
+	for (size_t i = 0; i < N; i++)
+	{
+		Parx[i] = 1.0f;
+		Pary[i] = 2.0f;
+	}
+	
+
+	auto start = high_resolution_clock::now();
+	add(N, x, y);
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	std::cout << "Time taken by non-parralel function: "
+		<< duration.count() << " microseconds" << std::endl;
+
+
+	start = high_resolution_clock::now();
+	
+
+	//<<<N,X>>> N = amount of threadblocks; X = amount of threads in a block;
+	parallelAdd<<<1,N>>>(N, x, y);
+	cudaDeviceSynchronize();
+	stop = high_resolution_clock::now();
+	duration = duration_cast<microseconds>(stop - start);
+	std::cout << "Time taken by parralel function: "
+		<< duration.count() << " microseconds" << std::endl;
+
+	delete[] x;
+	delete[] y;
+
+	//free cuda memory wait till kernel is done
+	cudaFree(Parx);
+	cudaFree(Pary);
+
+	return 0;
+
+}
--- CudaRasterizer/main.cpp
+++ CudaRasterizer/main.cpp
@@ -1,21 +1,181 @@
+#define GLEW_STATIC
 #include <iostream>
-#include <GLFW/glfw3.h>
-#include <gl/GL.h>
-#include <glm/glm.hpp>
+#include <glew.h>
+#include <glfw3.h>
+#include <glm.hpp>
 
-void InitGLFW();
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+#include "obj.h"
+
+//-------------------------------
+//------------Window-------------
+//-------------------------------
+int height = 800;
+int width = 800;
+
+//-------------------------------
+//------------GL STUFF-----------
+//-------------------------------
+int frame;
+int fpstracker;
+double seconds;
+int fps = 0;
+GLuint positionLocation = 0;
+GLuint texcoordsLocation = 1;
+const char* attributeLocations[] = { "Position", "Tex" };
+GLuint pbo = (GLuint)NULL;
+GLuint displayImage;
+uchar4* dptr;
+
+GLFWwindow* window;
+
+Obj* mesh;
+
+float* vbo;
+int vbosize;
+float* cbo;
+int cbosize;
+int* ibo;
+int ibosize;
+float* nbo;
+int nbosize;
+//-------------------------------
+//----------SETUP----------------
+//-------------------------------
+bool InitFramework();
+void InitCuda();
+void InitBuffers();
+void InitTextures();
+
+void cleanupCuda();
+void deleteTexture(GLuint* tex);
+void deletePBO(GLuint* pbo);
+
+
+//------------------------------
+//-------GLFW CALLBACKS---------
+//------------------------------
+void mainLoop();
+
+
+void errorCallback(int error, const char* description);
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+
+
 
 int main() {
-	InitGLFW();
-	std::cout << "Hello world\n";
+	InitFramework();
+	std::cout << "Hello world\n";	
 }
-
-
-void InitGLFW()
+//Init Functions
+bool InitFramework()
 {
+	glfwSetErrorCallback(errorCallback);
 	if (!glfwInit())
 	{
 		// Initialization failed
 		std::cout << "glfw failed initialization!\n";
 	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	window = glfwCreateWindow(width, height, "Horsecock ofcourse cock", NULL, NULL);
+	if (!window)
+	{
+		// Window or OpenGL context creation failed
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, keyCallback);
+
+	//glewExperimental = true;
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		/* Problem: glewInit failed, something is seriously wrong. */
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+	
+	}
+	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+	
+
+	// Set up GL context
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		return false;
+	}
+	// Initialize other stuff
+	InitBuffers();
+	InitTextures();
+	InitCuda();
+	InitBuffers();
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	return true;
+}
+
+void InitCuda() {
+	// Use device with highest Gflops/s
+	cudaGLSetGLDevice(0);
+
+	// Clean up on program exit
+	atexit(cleanupCuda);
+}
+void InitBuffers() {
+
+}
+void InitTextures() {
+	glGenTextures(1, &displayImage);
+	glBindTexture(GL_TEXTURE_2D, displayImage);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA,
+		GL_UNSIGNED_BYTE, NULL);
+}
+
+//Clean
+void cleanupCuda() {
+	if (pbo) deletePBO(&pbo);
+	if (displayImage) deleteTexture(&displayImage);
+}
+void deletePBO(GLuint* ppbo) {
+	if (ppbo) {
+		// unregister this buffer object with CUDA
+		cudaGLUnregisterBufferObject(*ppbo);
+
+		glBindBuffer(GL_ARRAY_BUFFER, *ppbo);
+		glDeleteBuffers(1, ppbo);
+
+		*ppbo = (GLuint)NULL;
+	}
+}
+
+void deleteTexture(GLuint* tex) {
+	glDeleteTextures(1, tex);
+	*tex = (GLuint)NULL;
+}
+
+
+//GLFW Callbacks
+void keyCallback(GLFWwindow* pwindow, int key, int, int action, int)
+{
+	//Set GLFW to close next frame
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(pwindow, GL_TRUE);
+	}
+}
+
+void errorCallback(int, const char* description)
+{
+	std::cout << description << std::endl;
+}
+void mainLoop()
+{
+
 }
